769e9ba3fea807695c2934fc9d11aa4b1988289b
David Ferreira <david-ferreira18@hotmail.com>
2020-06-07 21:04:07 +0100

    Fix hbase demo app.

--- example-applications/simple-hbase-application/src/main/java/Main.java
+++ example-applications/simple-hbase-application/src/main/java/Main.java
@@ -1,35 +1,19 @@
-import java.io.IOException;
-import java.util.Map;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
-
-import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        new App0(1);
-//        new App1("FirstTable");
-//        new App2();
-        Configuration config = HBaseConfiguration.create();
-        config.addResource("src/main/java/conf.xml");
-        Connection connection = ConnectionFactory.createConnection(config);
-        Admin admin = connection.getAdmin();
-        ClusterStatus clusterStatus = admin.getClusterStatus();
+    public static void main(String[] args) {
+        Db db = new Db();
 
-        for (ServerName serverName : clusterStatus.getServers()) {
-            ServerLoad serverLoad = clusterStatus.getLoad(serverName);
+        db.createTable(Fields.TABLE_NAME);
 
-            for (Map.Entry<byte[], RegionLoad> entry : serverLoad.getRegionsLoad().entrySet()) {
-                final String region = Bytes.toString(entry.getKey());
-                final RegionLoad regionLoad = entry.getValue();
-                long storeFileSize = regionLoad.getStorefileSizeMB();
-                System.out.println("Stored in " + region + ": " + storeFileSize);
-                // other useful thing in regionLoad if you like
-            }
-        }
+        db.scan(Fields.TABLE_NAME, new Scan());
+
+//        db.getWithGetOp(Fields.TABLE_NAME);
+//        db.get(Fields.TABLE_NAME, Bytes.toBytes("row1"));
+//        db.getWithFilterList(Fields.TABLE_NAME, Bytes.toBytes("row1"));
+//        db.getRecursive(Fields.TABLE_NAME, Bytes.toBytes("row1"));
+
+        db.deleteTable(Fields.TABLE_NAME);
     }
 
 }
